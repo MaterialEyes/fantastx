ced31a06323721b9b0debbe3392318673a832fee
Kendra Letchworth Weaver <letchwkl@jmu.edu>
2021-08-16 17:07:34 -0400

    Code added to merge with xrr_with_surf_dev branch

--- fx19/inputs.py
+++ fx19/inputs.py
@@ -23,6 +23,7 @@ from fx19 import experimental_simulation
 from fx19 import selection
 from fx19 import structure_operations
 
+import os
 
 def make_objects(i_dict):
     """
@@ -138,6 +139,8 @@ def make_objects(i_dict):
         basinhopping_params = i_dict['basinhopping_constraints']
     # NOTE: contains  'indices_fraction' and 'max_perturbation'
     basinhopping_params['min_dist_dict'] = str_constraints['min_dist_dict']
+    basinhopping_params['max_dist_dict'] = str_constraints['max_dist_dict']
+    basinhopping_params['element_syms'] = str_constraints['element_syms']
     basinhopping_params['species_dict'] = i_dict['structure_record']['species']
     basinhopping_params['shape'] = str_constraints['shape']
     if str_constraints['shape'] == 'cluster':
@@ -165,8 +168,18 @@ def make_objects(i_dict):
     # For surface layer searches, create surface_ops object
     surface_ops_obj = None
     if str_constraints['shape'] == 'surface':
+        init_slabs_path = str_record['surface']['init_slabs_dir']
+        init_slabs_dict = {i: init_slabs_path + '/' + slab_file \
+                    for i, slab_file in enumerate(os.listdir(init_slabs_path))}
+        str_constraints['init_slabs_dict'] = init_slabs_dict
+
         surface_ops_obj = structure_operations.surface_ops(hop, str_constraints)
         all_objects['surface_ops_obj'] = surface_ops_obj
+
+        energy_code.substrate_thickness = surface_ops_obj.substrate_thickness
+        energy_code.sd_cut_off = surface_ops_obj.sd_cut_off
+        energy_code.sd_no_z = surface_ops_obj.sd_no_z
+        all_objects['energy_code'] = energy_code
 
     ################### Develop any other below objects
 
@@ -329,6 +342,7 @@ def get_mating_params(i_dict, str_constraints):
     mating_params['species_dict'] = i_dict['structure_record']['species']
     mating_params['num_species'] = str_constraints['num_species']
     mating_params['shape'] = str_constraints['shape']
+    mating_params['element_syms'] = str_constraints['element_syms']
     if mating_params['shape'] == 'cluster':
         mating_params['box_abc'] = str_constraints['box_abc']
         mating_params['max_dia'] = str_constraints['max_dia']
@@ -361,11 +375,6 @@ def get_evolve_params(i_dict, str_constraints):
             if specie in str_constraints:
                 evolve_params[specie] = str_constraints[specie]
     return evolve_params
-
-def get_surface_params(i_dict, str_constraints):
-    """
-    Adds all required parameters for surface_ops class from the input file
-    """
 
 # assume experimental pdf is given (not implemented)
 def read_input_exp_files(filename):
