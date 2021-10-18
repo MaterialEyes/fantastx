f7d37d49cd37128165f9302899f87fd4ce2ce8fb
VS Chaitanya Kolluru <kvs.chaitanya@ufl.edu>
2021-10-18 10:50:59 -0500

    Added check_uniqueness after relaxation in full_eval

--- files/run_fx.py
+++ files/run_fx.py
@@ -159,6 +159,13 @@ def full_eval(model):
 
     # separate gb_iface for the energy evaluated futures
     separate_gb(energy_code, gb_ops_obj, model)
+    # check if the relaxed structure is unique
+    model_is_unique = pool.comparator.check_uniqueness(new_model,
+                                        pool.all_models, exact=False)
+    if not model_is_unique:
+        print ('Model {} is removed as it is not unique'.format(model.label))
+        return None
+
     # Do Xsim if required
     if Xsim_1:
         # get the relaxed structure
